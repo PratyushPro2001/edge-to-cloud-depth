d207c72b2a5e65595e38bf1663dbfefb7b3421bf
PratyushPro2001 <pratyush402@gmail.com>
2025-08-03 23:56:42 +0530

    Renamed the .ipynb file and slightly modifies the code to be readable.

--- src/client.py
+++ src/client.py
@@ -2,10 +2,8 @@ import cv2
 import requests
 import numpy as np
 
-# Replace with your actual ngrok URL (with /predict)
-SERVER_URL = "https://a40a9362e082.ngrok-free.app/predict"
+SERVER_URL = "https://9f2516dc7e7b.ngrok-free.app/predict"
 
-# Open webcam
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -13,30 +11,20 @@ while True:
     if not ret:
         break
 
-    # Resize to reduce upload + display size
     frame = cv2.resize(frame, (500, 300))
-
-    # Encode image as JPG
     _, img_encoded = cv2.imencode('.jpg', frame)
 
     try:
-        # Send to cloud server
         response = requests.post(SERVER_URL, files={"image": img_encoded.tobytes()})
 
         if response.ok:
-            # Decode depth map
             depth_map = cv2.imdecode(
                 np.frombuffer(response.content, np.uint8),
                 cv2.IMREAD_COLOR
             )
 
-            # Resize depth to match webcam size
             depth_map = cv2.resize(depth_map, (500 , 320))
-
-            # Stack vertically (webcam on top, depth below)
             combined = cv2.vconcat([frame, depth_map])
-
-            # Show the stacked result
             cv2.imshow("Depth Viewer", combined)
 
         else:
